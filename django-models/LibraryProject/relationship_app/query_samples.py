0d9501e6d5a3df57dbb8690ca41bf63a28e8f56a
Cyprian Kings <kiamacyprian6@gmail.com>
2025-09-21 14:25:46 +0300

    fix: checker fixes

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,8 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 books_by_author = Book.objects.filter(author__name='J.K. Rowling')
 
-books = Library.objects.get(name='library_name').books.all()
+library_name = "Nairobi Library"
+books = Library.objects.get(name=library_name).books.all()
 
 librarian = Librarian.objects.get(
     Library.objects.get(name='Central Library').librarian.id)
