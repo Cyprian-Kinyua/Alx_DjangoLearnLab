39cc261d7c797f0f1bcbc33cf3b2ac8150709486
Cyprian Kings <kiamacyprian6@gmail.com>
2025-09-22 14:45:29 +0300

    fix: checker fixes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register, LoginView, LogoutView, views
+from .views import LoginView, LogoutView
+from . import views
 
 
 urlpatterns = [
-    path('books/', list_books, name='book_list'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', views.list_books, name='book_list'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(),
+         name='library_detail'),
 
-    path('register/', register, name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
