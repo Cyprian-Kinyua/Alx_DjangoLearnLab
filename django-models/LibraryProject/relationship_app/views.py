e218a46fb6283d3ca04639df3aa8b618c87ff3b2
Cyprian Kings <kiamacyprian6@gmail.com>
2025-09-22 15:42:05 +0300

    Feat: Implementing role-based access control in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import user_passes_test, login_required
 
 # Create your views here.
 
@@ -38,3 +39,30 @@ class LoginView(LoginView):
 
 class LogoutView(LogoutView):
     template_name = 'relationship_app/logout.html'
+
+
+def is_admin(user):
+    return hasattr(user, 'profile') and user.profile.role == 'Admin'
+
+
+def is_librarian(user):
+    return hasattr(user, 'profile') and user.profile.role == 'Librarian'
+
+
+def is_member(user):
+    return hasattr(user, 'profile') and user.profile.role == 'Member'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
