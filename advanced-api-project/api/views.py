aace126f5f502b7a7b050661fefd1989a3ab7ce4
Cyprian Kings <kiamacyprian6@gmail.com>
2025-10-05 22:24:54 +0300

    feat: Building custo views and generic views in django rest framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from .models import Book
+from rest_framework import generics, permissions
+from .serializers import BookSerializer
 
 # Create your views here.
+# List all books (anyone can view)
+
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+# Retrieve one book by ID (Anyone can view)
+
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+# Create a new book (only authenticated users)
+
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Update an existing book (only authenticated users)
+
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Delete a book (only authenticated users)
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
